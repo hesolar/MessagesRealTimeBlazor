1e9d6baf27eb0546bfedc1eec7da36c16488630b
hsolar <hectorsolar99@gmail.com>
2021-08-17 11:49:06 +0200

    Agregar archivos de proyecto.

---
+++ Pages/IMessageQuery.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Mensajes.Pages {
+    public class IMessageQuery :INotifyPropertyChanged {
+        private StreamReader reader ;
+        private StreamWriter writer;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public String FileName { get; set; }
+        public List<String> Messages { get; set; }
+
+
+        public IMessageQuery(String FileName ) {
+            
+            this.FileName = FileName;
+            Messages = RetrieveAll();
+            
+        }
+     
+
+        public List<String> AddMessage(String s ) {
+            writer = new(FileName,append:true);
+            writer.Write(DateTime.Now.TimeOfDay + ":" + s+" ");
+            writer.Close();
+            
+            return RetrieveAll();
+        }
+
+        public List<String> RetrieveAll() {
+           
+            reader = new(FileName);
+            
+            var x = reader.ReadToEnd();
+            Messages = x.Split(" ").ToList();
+            reader.Close();
+            if( Messages.Count <= 1 ) return new();
+            Messages.RemoveAt(Messages.Count - 1);
+            return this.Messages;
+        }
+
+    }
+}
---
+++ Pages/Index2.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mensajes.Pages {
+    public class Index2Base: ComponentBase {
+
+        [Parameter] public  IMessageQuery query { get; set; }
+    public String newMsg = "";
+    public List<String> Mensajes = new();
+
+    public void sendMessage() {
+        query.AddMessage(newMsg);
+        Mensajes = query.RetrieveAll();
+        InvokeAsync(StateHasChanged);
+    }
+
+    protected override void OnParametersSet() {
+
+        query.PropertyChanged += ( e,f ) => InvokeAsync(() => StateHasChanged());
+        Mensajes = query.RetrieveAll();
+    }
+
+
+    }
+}
